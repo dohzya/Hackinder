4c9d0a11eb28c66748c247796951d351a0a00e74
Paul Dijou <pdi@zenexity.com>
2014-04-25 13:52:23 +0200

    One page + toggle form

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,20 +1,56 @@
 /** @jsx React.DOM */
 
-var Title = React.createClass({
-  render: function() {
-    return <h1>∞∞∞ {this.props.value} ∞∞∞</h1>;
-  }
+document.addEventListener('submit', function (event) {
+  event.preventDefault();
 });
 
-React.renderComponent(<Title value="Hackinder" />, document.getElementById('title'));
+$(".main").onepage_scroll({
+  sectionContainer: "section",
+  easing: "ease",
+  animationTime: 1000,
+  pagination: true,
+  updateURL: false
+});
 
 var Router = Abyssa.Router,
     State = Abyssa.State;
 
 Router({
-  home: State('/', {
-    enter: function () {
-      console.log('Enter home');
+  home: State('/?create&edit&slide', {
+    data: {
+      create: false,
+      edit: false,
+      slide: 0
+    },
+    enter: function (params) {
+      this.data('create', params.create);
+      this.data('edit', params.edit);
+      this.data('slide', params.slide);
+
+      toggleCreate(params.create);
+      // toggleEdit(params.edit);
+      // slideTo(params.slide);
+    },
+    update: function (params) {
+      console.log('Update page');
+
+      // CREATE PROJECT
+      if (params.create !== this.data('create')) {
+        this.data('create', params.create);
+        toggleCreate(params.create);
+      }
+
+      // EDIT PROJECT
+      if (params.edit !== this.data('edit')) {
+        this.data('edit', params.edit);
+        toggleEdit(params.edit);
+      }
+
+      // PROJECTS SLIDER
+      if (params.slide !== this.data('slide')) {
+        this.data('slide', params.slide);
+        slideTo(params.slide);
+      }
     }
   })
 })
@@ -23,4 +59,3 @@ Router({
   notFound: '/'
 })
 .init();
-
